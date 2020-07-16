ae870624334f175ef9d8c4268cef41954050c9f5
christan-duplicate <tyh@euiai.com>
2020-07-16 20:18:02 +0800

    Update on-complete.sh
    去掉 --leave-root，传输完成后把目录也删除

--- on-complete.sh
+++ on-complete.sh
@@ -34,7 +34,7 @@ if [[ $2 -eq 1 ]]; then # single file
 elif [[ $2 -gt 1 ]]; then # multiple file
 	echo "Transfering ${folderName} to Destination"
 	rclone -v --delete-empty-src-dirs --local-no-check-updated --ignore-existing --config="rclone.conf" move "$folderPath" "DRIVE:$RCLONE_DESTINATION/${folderName}" 2>&1
-	rclone --config="rclone.conf" rmdirs --leave-root "${folderPath}" 2>&1
+	rclone --config="rclone.conf" rmdirs "${folderPath}" 2>&1
 fi
 
 echo "$(($(cat numUpload)-1))" > numUpload # Minus 1
